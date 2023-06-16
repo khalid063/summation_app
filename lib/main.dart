aea4e406d0b43ad4479043968fa50f1e027fcc13
KhalidPC <khalid.ahmad063@gmail.com>
2023-06-16 12:01:32 +0600

    Design done

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int summationResult = 0;
+  int firstNumber = 0;
+  int secondNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sum App"),
+        title: const Text("Sum App"),
         centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 50, 20, 50),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                "sumation Result = $summationResult",
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.red
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter First Number',
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter Second Number',
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
